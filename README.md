421561d3bd4a384ad9e2e5b49a429057073d1b5d
jstudenski <jstudenski@gmail.com>
2017-11-27 20:14:48 -0600

    finally working (fixed ipcMain typo)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Electron-videoinfo
 Using Electron to build a cross platform desktop app
+
+### Start Up
+npm run electron
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
-const { app, BrowserWindow, icpMain } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
 
 // when the app is ready
 app.on('ready', () => {
@@ -8,7 +9,7 @@ app.on('ready', () => {
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-icpMain.on('video:submit', (event, path) => {
+ipcMain.on('video:submit', (event, path) => {
 	ffmpeg.ffprobe(path, (err, metadata) => {
 		console.log('Video duration is:', metadata.format.duration);	
 	});
