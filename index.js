b115d46c91b0052bedab16da57456c8640d3d1d1
jstudenski <jstudenski@gmail.com>
2017-11-27 20:29:42 -0600

    error when trying to pass info between Electron App and mainWindow

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const { app, BrowserWindow, ipcMain } = electron;
 
+// declare variable
+let mainWindow;
 
 // when the app is ready
 app.on('ready', () => {
-	const mainWindow = new BrowserWindow({});
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	// make new window
+	mainWindow = new BrowserWindow({}); 
+	// load window
+	mainWindow.loadURL(`file://${__dirname}/index.html`); 
 });
 
+// watch for video:submit from mainWindow
 ipcMain.on('video:submit', (event, path) => {
+	// get video metadata
 	ffmpeg.ffprobe(path, (err, metadata) => {
-		console.log('Video duration is:', metadata.format.duration);	
+		// send metadata back to mainWindow
+		mainWindow.webContents.send('video:metadata', metadata.format.duration);	
 	});
 }); 
